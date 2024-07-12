969fe60e5779837cf2ecf5b35c883dbd8c52007d
matt2d23 <127718477+matt2d23@users.noreply.github.com>
2024-07-11 22:08:21 -0500

    Added button to redraw grid, and checks to ensure value is <100

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const container = document.querySelector("#container");
+const button = document.querySelector(".btn");
 
 
 function createDiv(text) {
@@ -10,18 +11,18 @@ function createDiv(text) {
     return square;
 }
 
-function fillDivs() {
+function fillDivs(input) {
     let divs = [];
 
-    for (let i = 0; i < 256; i++) {
+    for (let i = 0; i < input; i++) {
         divs.push(createDiv(""));
     }
 
     return divs;
 }
 
-function appendDivs() {
-    let divs = fillDivs();
+function initializeDivs() {
+    let divs = fillDivs(256);
     const docFragment = document.createDocumentFragment();
 
     for (let i = 0; i < divs.length; i++) {
@@ -31,13 +32,29 @@ function appendDivs() {
     container.appendChild(docFragment);
 }
 
+function appendDivs(input) {
+    removeDivs();
+    let divs = fillDivs(input);
+    const docFragment = document.createDocumentFragment();
 
+    for (let i = 0; i < divs.length; i++) {
+        docFragment.appendChild(divs[i]);
+    }
+
+    container.appendChild(docFragment);
+}
+
+function removeDivs() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
 
 
 
 addEventListener("load", (e) => {
     console.log("page is loaded");
-    appendDivs();
+    initializeDivs();
     function divHover() {
         const getSquare = document.querySelectorAll(".square");
         getSquare.forEach((square) => {
@@ -50,4 +67,31 @@ addEventListener("load", (e) => {
         })
     }
     divHover();
+
+
+    button.addEventListener("click", () => {
+        let answer = +prompt("Enter a number to reform the grid (Maximum value 100)");
+        let answerSquared = answer * answer;
+        console.log(answer);
+        if (answer > 100) {
+            alert("Please enter a value lower than 100");
+        } else if (answer == false || answer === null) {
+            alert("Canceled");
+        } else {
+            appendDivs(answerSquared);
+            divHover();
+            let el = container.firstChild;
+            let i = 0;
+            while (i < container.childNodes.length) {
+                if (el === container.firstChild) {
+                    el.style.width = `calc(720px / ${answer})`
+                    el = el.nextSibling;
+                } else {
+                    el.style.width = `calc(720px / ${answer})`
+                    el = el.nextSibling;
+                }
+                i++;
+            }
+        }
+    })
 });
